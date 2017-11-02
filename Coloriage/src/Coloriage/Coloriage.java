9cb5c1fc988f27807960417b3627767b2f09993e
elmotard <elmotard@users.noreply.github.com>
2017-11-02 15:45:39 +0100

    Ajout fichier projet

---
+++ Coloriage/src/Coloriage/Coloriage.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Coloriage;
+
+/**
+ *
+ * @author Rémy Yagué
+ */
+public class Coloriage {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Coloriage_Frame cf = new Coloriage_Frame();
+        cf.setVisible(true);
+    }
+    
+}
